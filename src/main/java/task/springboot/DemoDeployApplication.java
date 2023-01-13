75171b8d090886275439109a8abdce4aeb23b7cf
arizki-25 <arizki.ynwa@gmail.com>
2023-01-13 11:39:40 +0800

    baru update

--- src/main/java/task/springboot/DemoDeployApplication.java
+++ src/main/java/task/springboot/DemoDeployApplication.java
@@ -15,7 +15,7 @@ public class DemoDeployApplication {
 
 	@GetMapping("/")
 	public String home(){
-		return "baru update";
+		return "baru update rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr";
 	}
 
 }
